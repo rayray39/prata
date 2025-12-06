999ba4d2752aeeeec91944c455b9b82303984ccb
rayray39 <raynerlimfy@gmail.com>
2025-12-06 22:36:53 +0800

    Add about section

---
+++ src/about/about.tsx
@@ -0,0 +1,13 @@
+
+export function About() {
+    return (
+        <div className="flex flex-col">
+            <div className="text-2xl">About the chef</div>
+
+            <div className="text-justify">
+                I'm a junior software engineer currently working in the industry.
+                I enjoy working on fullstack web development, powering applications using AI solutions, extracting insightful information from unstructured data using AI, and creating cute little web applications just for fun!
+            </div>
+        </div>
+    )
+}
