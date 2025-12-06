bc17b8eb7d2135ecc2e91ed0ca63f3db56d8c69d
rayray39 <raynerlimfy@gmail.com>
2025-12-06 22:37:14 +0800

    Add contact section

---
+++ src/contact/contact.tsx
@@ -0,0 +1,19 @@
+
+export function Contact() {
+    return (
+        <div className="flex flex-col">
+            <div className="text-2xl">Make a reservation</div>
+
+            <div className="flex flex-col justify-center items-center">
+                <div className="flex items-center">
+                    <img className="w-5 h-5 mr-2" src="linkedin_blue.png" alt="linkedin" />
+                    <a href="https://www.linkedin.com/in/rayray39/">rayray39</a>
+                </div>
+                <div className="flex items-center mt-2">
+                    <img className="w-5 h-5 mr-2" src="github_new.png" alt="github" />
+                    <a href="https://www.github.com/rayray39">rayray39</a>
+                </div>
+            </div>
+        </div>
+    )
+}
