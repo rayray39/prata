e97dc182d2aea6811ad1cdbfc9890b90498351f1
rayray39 <raynerlimfy@gmail.com>
2025-12-06 22:37:33 +0800

    Add projects section

---
+++ src/projects/projects.tsx
@@ -0,0 +1,10 @@
+
+export function Projects() {
+    return (
+        <div className="flex flex-col">
+            <div className="text-2xl">Prata menu</div>
+
+            <div className="text-center">*** Placeholder ***</div>
+        </div>
+    )
+}
