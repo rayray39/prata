eb81216be1b517160384be8f194751fb3f550fd1
rayray39 <raynerlimfy@gmail.com>
2025-12-06 15:37:04 +0800

    Add linkedin link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,17 @@
 export default function Home() {
   return (
     <div>
-        <div
-            className="flex justify-center items-center h-screen"
-        >Prata Developer</div>
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="text-3xl">Prata Developer</div>
+            <div className="flex items-center">
+                <div>By Rayner</div>
+                <div>
+                    <a href="https://www.linkedin.com/in/rayray39/">
+                        <img className="w-5 h-5 ml-2" src="linkedin_blue.png" alt="linkedin" />
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
