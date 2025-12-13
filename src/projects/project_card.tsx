c22503b13bd01691e0c2d94466250f3b3b52e4b0
rayray39 <raynerlimfy@gmail.com>
2025-12-13 17:17:26 +0800

    Update project card styles for light and dark themes

--- src/projects/project_card.tsx
+++ src/projects/project_card.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useTheme } from "next-themes";
+import "../app/globals.css";
 
 interface ProjectHeader {
     emoji:string,
@@ -18,18 +21,24 @@ export function ProjectCard({
     description,
     techStack
 }:ProjectDetails) {
+    const { theme } = useTheme();
+
     return (
-        <div className="border rounded-lg p-4 mt-4">
+        <div className={theme === "light" ? "light-project-card" : "dark-project-card"}>
             <li className="list-none">
                 <div className="flex justify-between items-center">
                     <div className="text-lg">{header.emoji}</div>
-                    <div>
+                    <div className="grid grid-cols-2 gap-1 items-center">
                         <div>
                             <a href={header.githubLink}>
                                 <img className="w-5 h-5" src="github_new.png" alt="github"/>
                             </a>
                         </div>
-                        <div>{header.projectLink}</div>
+                        <div>
+                            <a href={header.projectLink}>
+                                📌
+                            </a>
+                        </div>
                     </div>
                 </div>
     
