cb30950d50aed10ce109ec9ec5d601964fa9bda0
rayray39 <raynerlimfy@gmail.com>
2025-12-13 16:47:08 +0800

    Add project cards to project section

--- src/projects/projects.tsx
+++ src/projects/projects.tsx
@@ -1,10 +1,44 @@
+import { ProjectCard } from "./project_card";
 
 export function Projects() {
     return (
         <div className="flex flex-col">
-            <div className="text-2xl">Prata menu</div>
+            <div className="text-2xl">Menu</div>
 
-            <div className="text-center">*** Placeholder ***</div>
+            <ul className="grid grid-cols-3 gap-2">
+                <ProjectCard 
+                    header={{
+                        emoji: "🍨",
+                        githubLink: "https://github.com/rayray39/chendol",
+                        projectLink: ""
+                    }}
+                    title="Chendol AI"
+                    description="some placeholder text"
+                    techStack="python"
+                />
+
+                <ProjectCard 
+                    header={{
+                        emoji: "🍜",
+                        githubLink: "",
+                        projectLink: ""
+                    }}
+                    title="HokkienMee"
+                    description="some placeholder text"
+                    techStack="java"
+                />
+
+                <ProjectCard 
+                    header={{
+                        emoji: "🥘",
+                        githubLink: "",
+                        projectLink: ""
+                    }}
+                    title="CharKwayTeow"
+                    description="some placeholder text"
+                    techStack="javascript"
+                />
+            </ul>
         </div>
     )
 }
