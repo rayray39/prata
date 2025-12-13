28dcbf6fbbb5129e6f6e933a8429e40f557fafeb
rayray39 <raynerlimfy@gmail.com>
2025-12-13 16:46:06 +0800

    Update margins for contacts

--- src/contact/contact.tsx
+++ src/contact/contact.tsx
@@ -5,11 +5,11 @@ export function Contact() {
             <div className="text-2xl">Make a reservation</div>
 
             <div className="flex flex-col justify-center items-center">
-                <div className="flex items-center">
+                <div className="flex items-center mt-4">
                     <img className="w-5 h-5 mr-2" src="linkedin_blue.png" alt="linkedin" />
                     <a href="https://www.linkedin.com/in/rayray39/">rayray39</a>
                 </div>
-                <div className="flex items-center">
+                <div className="flex items-center mt-4">
                     <img className="w-5 h-5 mr-2" src="github_new.png" alt="github" />
                     <a href="https://www.github.com/rayray39">rayray39</a>
                 </div>
