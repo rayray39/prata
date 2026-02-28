2f7ac3132f8bdf9f6409109dd47975ea9baf83e9
rayray39 <raynerlimfy@gmail.com>
2026-02-28 13:09:48 +0800

    Update kitchen utensils with new tech stack icons

--- src/tech_stack/tech_stack.tsx
+++ src/tech_stack/tech_stack.tsx
@@ -1,45 +1,11 @@
 'use client'
-import StackIcon from "tech-stack-icons";
-import { TechSection } from "./tech_section";
 import { useTheme } from "next-themes";
 import { useState } from "react";
 
-const data_eng_and_ai_list = [
-    {name:"LangGraph"},
-    {name:"Huggingface"},
-    {name:"vLLM"},
-    {name:"Ollama"},
-    {name:"Elasticsearch"},
-    {name:"OpenRouter"}
-]
 
-const fullstack_list = [
-    {name:"Python", iconName:"python"},
-    {name:"React (TypeScript)", iconName:"typescript"},
-    {name:"NextJS", iconName:"nextjs"},
-    {name:"ExpressJS", iconName:"expressjs"},
-    // {name:"JavaScript"},
-    // {name:"ExpressJS"},
-    // {name:"PostgreSQL"},
-]
-
-const others_list = [
-    {name:"Git"},
-    {name:"Docker"},
-    {name:"Linux"},
-    {name:"Tailwind CSS"},
-    {name:"Supabase"}
-]
-
-function originalTechStack() {
-    return <ul className="grid grid grid-cols-1 md:grid-cols-3 gap-2">
-        {/* <TechSection header="Data Engineering/AI" listOfTechCards={data_eng_and_ai_list}/> */}
-
-        <TechSection header="FullStack" listOfTechCards={fullstack_list}/>
-
-        {/* <TechSection header="Others" listOfTechCards={others_list}/> */}
-    </ul>
-}
+const fullstack_list:string = "js,python,java,postgres";
+const frameworks_list:string = "ts,nextjs,express,django,tailwind";
+const tools_list:string = "huggingface,ollama,langchain,vercel,docker,elasticsearch";
 
 export function TechStack() {
     const { theme } = useTheme();
@@ -48,12 +14,12 @@ export function TechStack() {
         <div>
             <div className="text-xl md:text-2xl">Kitchen Utensils</div>
 
-            <div className="grid grid-cols-1 gap-y-8">
+            <div className="grid grid-cols-1 md:gap-y-8 sm:gap-y-4">
                 <div className="flex flex-col items-center">
                     <div>Fullstack</div>
                     <p> 
                         <a href="https://skillicons.dev">
-                            <img src="https://skillicons.dev/icons?i=js,python,java,postgres&theme=dark" />
+                            <img src={`https://skillicons.dev/icons?i=${fullstack_list}&theme=dark`} />
                         </a>
                     </p>
                 </div>
@@ -62,7 +28,7 @@ export function TechStack() {
                     <div className="text-center">Frameworks</div>
                     <p>
                         <a href="https://skillicons.dev">
-                            <img src="https://skillicons.dev/icons?i=ts,nextjs,express,django,tailwind&theme=dark" />
+                            <img src={`https://skillicons.dev/icons?i=${frameworks_list}&theme=dark`} />
                         </a>
                     </p>
                 </div>
@@ -71,7 +37,7 @@ export function TechStack() {
                     <div className="text-center">Tools</div>
                     <p>
                         <a href="https://go-skill-icons.vercel.app">
-                            <img src="https://go-skill-icons.vercel.app/api/icons?i=huggingface,ollama,langchain,vercel,docker,elasticsearch&theme=dark" />
+                            <img src={`https://go-skill-icons.vercel.app/api/icons?i=${tools_list}&theme=dark`} />
                         </a>
                     </p>
                 </div>
